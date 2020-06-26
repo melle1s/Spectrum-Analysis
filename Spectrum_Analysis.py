a64ecaf8244ae077e83e2297665963fdbe38c103
Melvin Storbacka <melvin@melvin.se>
2020-06-26 10:20:15 +0200

    not currently working, I will implement pandas later to get it working (and faster)

--- Spectrum_Analysis.py
+++ Spectrum_Analysis.py
@@ -1,1 +1,23 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
+with open("csvSpectrum1.txt", "r") as test:
+    rawData = test.readlines()
+test.close()
+dataProcessed = []
+for x in range(1, len(rawData)):
+    line = rawData[x].split('\n')
+    line.pop(1)
+    for data in line:
+        finalData = data.split(',')
+        dataProcessed.append([finalData[0], finalData[2]])
+x = []
+y = []
+
+for point in dataProcessed:
+    x.append(point[0])
+    y.append(point[1])
+    
+plt.plot(x, y)
+plt.savefig("plot1.png")
